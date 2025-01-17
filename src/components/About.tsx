db3d84f7a5973d7921600698c080d86652b675bf
avoytovich <levanwork@gmail.com>
2025-01-17 08:15:01 +0100

    correct: about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ const About = () => (
     <div className="container mx-auto px-4">
       <h2 className="text-3xl font-bold mb-12 text-center">About Me</h2>
       <p className="text-gray-700 mb-6 text-center">
-        I’m a Full-stack (JS/TS/React & Node/Postgres) developer developer with 6 years in the IT industry
+        I’m a React (front-end) developer developer with 6 years in the IT industry
       </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Address */}
